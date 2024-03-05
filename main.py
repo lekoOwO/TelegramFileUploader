495e9e79170d21a500aa42504a377bc57c6d382d
xz-dev <xiangzhedev@gmail.com>
2024-03-05 15:27:35 +0800

    fix: keep same as telegram api send file

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
-from os import environ
 from argparse import ArgumentParser
+from os import environ
 from pathlib import Path
 
 from telethon import TelegramClient
@@ -37,17 +37,25 @@ async def main(client: TelegramClient, to: str, message: str, files: list[str]):
         uploaded_files.append(ufile)
 
     print(f"Sending message")
+    message_list = [None for i in range(len(uploaded_files) - 1)]
+    message_list.append(message)
     await client.send_file(
-        entity=to, file=uploaded_files, caption=message, progress_callback=callback
+        entity=to, file=uploaded_files, caption=message_list, progress_callback=callback
     )
     print(f"Sent message")
 
 
-parser = ArgumentParser(prog="TelegramFileUploader", epilog="@GitHub:xz-dev")
-parser.add_argument("--to", help="Chat ID or username")
-parser.add_argument("--message", help="Message")
-parser.add_argument("--files", help="Files", nargs="+")
+def get_arg_parser():
+    parser = ArgumentParser(prog="TelegramFileUploader", epilog="@GitHub:xz-dev")
+    parser.add_argument("--to", help="Chat ID or username")
+    parser.add_argument("--message", help="Message")
+    parser.add_argument("--files", help="Files", nargs="+")
+    return parser
+
+
+parser = get_arg_parser()
 args = parser.parse_args()
+
 with bot:
     bot.loop.run_until_complete(main(bot, args.to, args.message, args.files))
 
